10b3f718acb3d2c1d24f349439088f2e80db9d74
crist-start <criststart.cs@gmail.com>
2019-06-14 10:22:11 -0500

    tercer cambio

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Autos App' });
 });
 
 module.exports = router;
+
+router.get('/mazda',function(req,res,next) {
+	var infoMazda={}
+	infoMazda.pais='Japón'
+	infoMazda.sectorVentas='Comercial'
+	infoMazda.anioFundacion=1929
+	infoMazda.logo="https://www.mazda.mx/siteassets/mazda-mx/logos-new-mazda/mazda-logo-desktop-2.png"
+
+	res.render('mazda',infoMazda)
+})
